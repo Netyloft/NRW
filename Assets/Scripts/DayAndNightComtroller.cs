ff81c6ac5b0926847faaabcfcc759c38b0ace370
Netyloft <netesov.9882@gmail.com>
2021-05-21 21:10:50 +0500

    Добавил скрипт для смены времени суток

---
+++ Assets/Scripts/DayAndNightComtroller.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class DayAndNightComtroller : MonoBehaviour
+{
+    [SerializeField] private Gradient directionalLightGradient;
+    [SerializeField] private Gradient ambientLightGradient;
+    [SerializeField, Range(1, 3600)] private float timeDayInSeconds = 60;
+    [SerializeField, Range(0f, 1f)] private float timeProgress;
+    [SerializeField] private Light directLight;
+
+    private Vector3 defaultAngless;
+    void Start()
+    {
+        defaultAngless = directLight.transform.localEulerAngles;
+    }
+    
+    void Update()
+    {
+        if (Application.isPlaying)
+            timeProgress += Time.deltaTime / timeDayInSeconds;
+
+        if (timeProgress > 1f)
+            timeProgress = 0f;
+
+        directLight.color = directionalLightGradient.Evaluate(timeProgress);
+        RenderSettings.ambientLight = ambientLightGradient.Evaluate(timeProgress);
+        
+        directLight.transform.localEulerAngles = new Vector3(360f * timeProgress - 90, defaultAngless.x, defaultAngless.z);
+    }
+}
